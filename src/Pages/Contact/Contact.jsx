1090956ab88097acbea754cb6da3aad18d3788ad
Paul <paul.m.donovan@outlook.com>
2025-02-24 21:47:49 +1100

    added products

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -36,8 +36,8 @@ function Contact() {
                         <input type="email" name="user_email" />
                         <label>Message</label>
                         <textarea name="message" />
-                        <input type="submit" value="Send" />
-                    </form>
+                        <input className="form-btn" type="submit" value="Send" />
+                    </form>     
                 </div>
 
                 <div className='form-page-img'>
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -14,24 +14,29 @@ const Products = () => {
       <p>It would be great if you woulc consider buying from my store. </p>
         </div>
         <div className="product-items">
-          <div className='product-list'>
+        <div className='product-list'>
+          <a href="https://www.amazon.com.au/Education-Collection-Stories-Paul-Donovan/dp/1504302494 " target="_blank" rel="noopener noreferrer">
           <h1>Education</h1> 
           <img src={book1} alt="Paul Donovan Book Cover" />
+          </a>          
 
           <p>A whimsical colection of short stories</p>
           </div>
           <div className='product-list'>
+          <a href="https://pauldonovan.gumroad.com/l/VqwrZx" target="_blank" rel="noopener noreferrer">
           <h1>The Reflection</h1> 
           <img src={book2} alt="Paul Donovan Book Cover" />
-
+          </a>
           <p>A small town. A murder. A new life. All is not well when John Moore moves to a small town to work at the local school.</p>
 
           </div>
           <div className='product-list'>
+            <a href="  https://pauldonovan.gumroad.com/l/NWO ">
+        
           <h1>New World Order</h1> 
           <img src={book3} alt="Paul Donovan Book Cover" />
-
-          <p>aMmid the brutal covid shutdowns, one man seeks peace and islatin from the madness</p>
+          </a>
+          <p>During the brutal covid lockdowns, one man seeks peace and freedom from the madness</p>
           </div>
 
         </div>
